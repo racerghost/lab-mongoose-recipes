b34e03449273bbc453813497b3116a35c8f42a03
Carlos Bausá Carmona <carlos@MacBook-Pro-de-Carlos.local>
2021-05-08 20:28:25 +0200

    iteratio 1&2 done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { db } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -21,7 +22,24 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    
+    // data.forEach(element => {
+    //   console.log ("Showed title before inserting: ",element.title);
+    //   Recipe.create (element);
+    // });
+    Recipe.insertMany(data)
+
+  })
+  .then(()=>{
+    Recipe.find({})
+      .then(oneRecipe => {
+        console.log(oneRecipe);
+      })
+      .catch(error => {
+        console.log('Error finding for recipes');
+      });
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+  
